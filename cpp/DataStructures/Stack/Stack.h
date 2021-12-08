b6f8f92c7644b848fb5d0b214a428701c5139d30
Que Nguyen <quenguyen10190@gmail.com>
2021-12-08 13:06:22 +0700

    feat: cpp add Stack interface, implement stack into ArrayStack

---
+++ cpp/DataStructures/Stack/Stack.h
@@ -0,0 +1,36 @@
+#ifndef ALGORITHMS_STACK_H
+#define ALGORITHMS_STACK_H
+
+#include <iostream>
+
+namespace dsa {
+    template<typename T>
+    class Stack {
+    private:
+        int id_;
+    public:
+        explicit Stack(int id) : id_(id) {}
+
+        virtual ~Stack() = default;
+
+        virtual int id() {
+            return id_;
+        }
+
+        virtual int size() = 0;
+
+        virtual void clear() = 0;
+
+        virtual bool isEmpty() = 0;
+
+        virtual void push(const T &element) = 0;
+
+        virtual T pop() = 0;
+
+        virtual T peek() = 0;
+
+        virtual int capacity() = 0;
+    };
+}
+
+#endif //ALGORITHMS_STACK_H
