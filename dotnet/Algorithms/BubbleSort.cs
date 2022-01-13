2fd867f05a0493b4f7828f4d04155462eceaf3ec
Que Nguyen <quenguyen10190@gmail.com>
2022-01-13 17:24:24 +0700

    feat: algorithm selection insertion bubble sort

---
+++ dotnet/Algorithms/BubbleSort.cs
@@ -0,0 +1,22 @@
+﻿namespace Algorithms;
+
+public class BubbleSort : ISort
+{
+    public int[] Sort(int[] input)
+    {
+        for (var i = 0; i < input.Length - 1; i++)
+        {
+            var swapped = false;
+            for (var j = 0; j < input.Length - i - 1; j++)
+            {
+                if (input[j] <= input[j + 1]) continue;
+                (input[j], input[j + 1]) = (input[j + 1], input[j]);
+                swapped = true;
+            }
+
+            if (swapped == false) break;
+        }
+
+        return input;
+    }
+}
---
+++ dotnet/Algorithms/InsertionSort.cs
@@ -0,0 +1,21 @@
+﻿namespace Algorithms;
+
+public class InsertionSort : ISort
+{
+    public int[] Sort(int[] input)
+    {
+        for (var i = 1; i < input.Length; i++)
+        {
+            var last = input[i];
+            var j = i;
+            while (j > 0 && input[j - 1] > last)
+            {
+                input[j] = input[j - 1];
+                j -= 1;
+                input[j] = last;
+            }
+        }
+
+        return input;
+    }
+}
